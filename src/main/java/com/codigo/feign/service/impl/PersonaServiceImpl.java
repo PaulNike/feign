3ffba377cce7fba1315e326392cbe000dc3a598f
NikeRodriguez <nike_1109@outlook.es>
2024-02-07 22:50:48 -0500

    feat: createPerson 1

--- src/main/java/com/codigo/feign/service/impl/PersonaServiceImpl.java
+++ src/main/java/com/codigo/feign/service/impl/PersonaServiceImpl.java
@@ -1,25 +1,31 @@
 package com.codigo.feign.service.impl;
 
 
+import com.codigo.feign.aggregates.constants.Constants;
 import com.codigo.feign.aggregates.request.PersonaRequest;
 import com.codigo.feign.aggregates.response.BaseResponse;
+import com.codigo.feign.aggregates.response.ReniecResponse;
 import com.codigo.feign.feignclient.ReniecClient;
 import com.codigo.feign.repository.PersonaRepository;
 import com.codigo.feign.service.PersonaService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PersonaServiceImpl implements PersonaService {
 
-    @Autowired
-    private  PersonaRepository personaRepository;
+    private final PersonaRepository personaRepository;
+    private final  ReniecClient reniecClient;
 
-    private final   ReniecClient reniecClient;
-
-    public PersonaServiceImpl(ReniecClient reniecClient) {
+    public PersonaServiceImpl(PersonaRepository personaRepository, ReniecClient reniecClient) {
+        this.personaRepository = personaRepository;
         this.reniecClient = reniecClient;
     }
+
+    @Value("token.api")
+    private String tokenApi;
 
     @Override
     public BaseResponse crearPersona(PersonaRequest personaRequest) {
@@ -28,6 +34,16 @@ public class PersonaServiceImpl implements PersonaService {
 
     @Override
     public BaseResponse getInfoReniec(String numero) {
-        return null;
+        ReniecResponse response = getExecution(numero);
+        if(response != null){
+            return new BaseResponse(Constants.CODE_SUCCESS,Constants.MESS_SUCCESS, Optional.of(response));
+        }else{
+            return new BaseResponse(Constants.CODE_ERROR,Constants.MESS_ERROR, Optional.empty());
+        }
+    }
+
+    private ReniecResponse getExecution(String numero){
+        String authorization = "Bearer "+tokenApi;
+        return reniecClient.getInfo(numero,authorization);
     }
 }
